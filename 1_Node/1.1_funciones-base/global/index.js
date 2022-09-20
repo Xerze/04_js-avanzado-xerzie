01e90bcd7ed25f93f813f8baaa924de5f610b4c9
Xerze (M. Granados) <xerzewyth@gmail.com>
2022-09-20 16:34:52 -0500

    feat: agrega funciones de global y os

--- 1_Node/1.1_funciones-base/global/index.js
+++ 1_Node/1.1_funciones-base/global/index.js
@@ -1,3 +1,7 @@
 console.log(this); // El this es Objeto vacío({}) o module.exports
 console.log("Ruta donde está mi archivo: ", __dirname);
 console.log("Ruta + nombre de mi archivo: ", __filename);
+
+// module: Referencia al módulo actual. 
+// exports: Para exportar módulos (archivos)
+// require(): Importa un archivo js como módulo. Ej; require("index.js");
--- 1_Node/1.1_funciones-base/os/index.js
+++ 1_Node/1.1_funciones-base/os/index.js
@@ -3,4 +3,22 @@
     // y lo guardamos en la constante os para que podamos usarlo más adelante.
 const os = require('os');
 
-console.log('Versión del sistema: ', os.version());
+// Muestra el sistema operativo instalado en la computadora.
+console.log('Versión detallada del sistema detallada: ', os.version());
+// Muestra la versión del sistema.
+console.log('Nombre de la versión del sistema: ', os.platform());
+// Muestra  la versión del sistema operativo.
+console.log('Numero de la versión del sistema:' + os.release());
+
+// Muestra la arquitectura en bits que ocupa el sistema (x32, x64, x86).
+console.log('Arquitectura: ',os.arch()); 
+// // Nos muestra el número de procesadores virtuales.
+console.log('Número de procesadores lógicos:' + os.cpus().length);
+// Nos muestra la información del Procesador, un arreglo.
+console.log('Información acerca de los cpus: ', os.cpus());
+// Muestra la memoria libre en bytes.
+console.log('Memoria libre en bytes', os.freemem());
+// Muestra cuál es la carpeta del usuario.
+console.log('Carpeta del usuario actual: ', os.homedir()); 
+// Muestra cuál es la dirección IP.
+console.log('Mi direccion ip o mi hostname (local): ', os.hostname()); 
