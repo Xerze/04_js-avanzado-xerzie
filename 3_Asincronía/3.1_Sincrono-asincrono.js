f3a4064e25a9807c86e7a74599fb592f82d8bc35
Xerze (M. Granados) <xerzewyth@gmail.com>
2022-09-24 19:32:29 -0500

    feat: problema de asincronia

---
+++ 3_Asincronía/3.1_Sincrono-asincrono.js
@@ -0,0 +1,16 @@
+/**
+ * BLOQUE 1 - Código sincrono
+ * Una linea se ejecuta inmediatamente despues de la otra
+ * no hay codigo dependiente o bloqueante.
+ */
+ 
+console.log('============== BLOQUE 1 ==============');
+console.log('Primero'); 
+console.log('Segundo');
+console.log('Tercero');
+
+/**
+ * BLOQUE 2 - Problema tipico de asincronia.
+ * Una linea se ejecuta inmediatamente despues de la otra
+ * no hay codigo dependiente o bloqueante.
+ */
